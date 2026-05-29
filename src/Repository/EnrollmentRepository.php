c0c29bd742b282b80fc9f74f33ebf0190f8b464d
Ramatoulaye Soumare <ramatoulaye.sou@gmail.com>
2026-05-30 01:27:17 +0200

    feat(enrollment): repo finders findByUser + findOneByUserAndFormation

--- src/Repository/EnrollmentRepository.php
+++ src/Repository/EnrollmentRepository.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\Repository;
 
 use App\Entity\Enrollment;
+use App\Entity\Formation;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -16,5 +18,30 @@ final class EnrollmentRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Enrollment::class);
+    }
+
+    /**
+     * @return Enrollment[]
+     */
+    public function findByUser(User $user): array
+    {
+        return $this->createQueryBuilder('e')
+            ->andWhere('e.user = :user')
+            ->setParameter('user', $user)
+            ->orderBy('e.createdAt', 'DESC')
+            ->addOrderBy('e.id', 'DESC')
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function findOneByUserAndFormation(User $user, Formation $formation): ?Enrollment
+    {
+        return $this->createQueryBuilder('e')
+            ->andWhere('e.user = :user')
+            ->andWhere('e.formation = :formation')
+            ->setParameter('user', $user)
+            ->setParameter('formation', $formation)
+            ->getQuery()
+            ->getOneOrNullResult();
     }
 }
